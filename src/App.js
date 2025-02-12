fec9889293aff96c59ed0f84e3a4344f94b7cca7
Galen Yuan <galenYuan@student.unsw.edu.au>
2025-02-13 00:23:27 +1100

    made some changes in app.js

--- src/App.js
+++ src/App.js
@@ -18,16 +18,18 @@ function App() {
   return (
     <Router>
       <div className="relative min-h-screen bg-[#1d1e24] flex flex-col">
+      <div className="absolute inset-0 z-0">
         <SvgDecoration />
         <SvgDecorationLeftBottom />
         <SvgDecorationRight />
+      </div>
 
         <div className="App flex justify-center bg-[#1d1e24] min-h-screen">
           <div className="max-w-screen-lg w-full">
             <Header />
             <div className="flex items-start mt-2">
               <Sidebar />
-              <div className="flex-1 p-8">
+              <div className="flex-1 p-8 pt-28 md:pt-8 bg-[#1d1e24] overflow-y-auto min-h-screen relative z-20">
                 <Routes>
                   <Route path="/" element={<About />} />
                   <Route path="/about" element={<About />} />
